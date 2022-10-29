a45167681afe5ca10549aa88e98e9dc63e1c588d
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2022-10-29 03:53:12 +0200

    Added documentations to methods of Bits. Added the structs BitsIterator and BitsIteratorConcume which implements the into iterator and iterator traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![deny(unsafe_code)]
 
+use paste::paste;
 use std::fmt::{Binary, Debug, Display, Formatter};
 
 #[derive(Debug)]
+/// Errors in the bit_rw crate.
 pub enum Error {
     IndexOutOfRange,
 }
@@ -38,12 +40,21 @@ macro_rules! cast_from_bit {
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Represents one bit.
 pub enum Bit {
     Zero,
     One,
 }
 
 impl Bit {
+    /// Inverts the bit.
+    ///
+    /// # Example
+    /// ```
+    /// use bit_rw::Bit;
+    ///
+    /// assert_eq!(Bit::One, Bit::Zero.invert())
+    /// ```
     pub fn invert(&self) -> Self {
         match self {
             Self::Zero => Self::One,
@@ -87,164 +98,344 @@ macro_rules! impl_from_and_into_for_bit {
 
 impl_from_and_into_for_bit! { u8 u16 u32 u64 u128 usize}
 
-macro_rules! impl_bits_for_size {
-    ($(($name:ident, $size:ty, $index_type:ty))*) => ($(
-
-    #[derive(Default)]
-    #[doc = concat!(
-        "Wrapper type of the primitive type `",
-        stringify!($size),
-        "` to be able to read/write to the bits."
-    )]
-    pub struct $name {
-        bits: $size,
-    }
-
-    impl $name {
-
-        #[doc = concat!("Creates a `", stringify!($name), "`")]
-        /// , the input `bits` is the bits that is going to be interacted with.
-        pub fn new(bits: $size) -> Self {
-            Self { bits }
-        }
-
-        /// Shifts all bits to the left and inserts the `bit` at the left most bit.
-        ///
-        /// Shifting with bit [`Bit::Zero`] is the same as a normal shift left.
-        ///
-        /// # Example
-        /// ```
-        #[doc = concat!("use bit_rw::{", stringify!($name), ", Bit};")]
-        ///
-        #[doc = concat!("let mut b = ", stringify!($name), "::new(0);   // 0b0")]
-        /// b.shift_left_with(Bit::One);  // 0b01
-        /// b.shift_left_with(Bit::One);  // 0b011
-        /// b.shift_left_with(Bit::Zero); // 0b0110
-        ///
-        /// assert_eq!(b, 6.into());
-        /// ```
-        pub fn shift_left_with(&mut self, bit: Bit) {
-            self.bits <<= 1;
-            if bit == Bit::One {
-                self.bits |= 1;
+macro_rules! impl_bits {
+    ($name:ident, $size:ty, $index_type:ty, $bit_len:literal) => {
+        paste!{
+            #[derive(Default)]
+            #[doc = "Wrapper type of the primitive type `" $size "` to be able to read/write to the bits."]
+            pub struct $name {
+                bits: $size,
             }
         }
 
-        /// Shifts all bits to the right and inserts the `bit` at the right most bit.
-        ///
-        /// Shifting with bit [`Bit::Zero`] is the same as a normal shift right.
-        ///
-        /// # Example
-        /// ```
-        #[doc = concat!("use bit_rw::{", stringify!($name), ", Bit};")]
-        ///
-        #[doc = concat!("let mut b = ", stringify!($name), "::new(0);  // 0b0")]
-        /// b.shift_right_with(Bit::One);     // 0b1000...
-        /// // b.shift_right_with(Bit::One);  // 0b11000...
-        /// // b.shift_right_with(Bit::Zero); // 0b011000...
-        ///
-        /// // assert_eq!(b, (0b011000...).into());
-        #[doc = concat!(
-            "assert_eq!(b, 2_",
-            stringify!($size),
-            ".pow(<",
-            stringify!($size),
-            ">::BITS-1).into());"
-        )]
-        /// ```
-        pub fn shift_right_with(&mut self, bit: Bit) {
-            self.bits >>= 1;
-            if bit == Bit::One {
-                self.bits |= 1 << (<$size>::BITS-1);
+        impl $name {
+            paste!{
+                #[doc = "Creates a [`" $name "`], the input `bits` is the bits that is going to be interacted with."]
+                pub fn new(bits: $size) -> Self {
+                    Self { bits }
+                }
+            }
+
+            paste!{
+                /// Shifts all bits to the left and inserts the `bit` at the left most bit.
+                ///
+                /// Shifting with bit [`Bit::Zero`] is the same as a normal shift left.
+                ///
+                /// # Example
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0);   // 0b0"]
+                /// b.shift_left_with(Bit::One);  // 0b01
+                /// b.shift_left_with(Bit::One);  // 0b011
+                /// b.shift_left_with(Bit::Zero); // 0b0110
+                ///
+                /// assert_eq!(b, 6.into());
+                /// ```
+                pub fn shift_left_with(&mut self, bit: Bit) {
+                    self.bits <<= 1;
+                    if bit == Bit::One {
+                        self.bits |= 1;
+                    }
+                }
+            }
+
+            paste!{
+                /// Shifts all bits to the right and inserts the `bit` at the right most bit.
+                ///
+                /// Shifting with bit [`Bit::Zero`] is the same as a normal shift right.
+                ///
+                /// # Example
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0);  // 0b0"]
+                /// b.shift_right_with(Bit::One);     // 0b1000...
+                /// // b.shift_right_with(Bit::One);  // 0b11000...
+                /// // b.shift_right_with(Bit::Zero); // 0b011000...
+                ///
+                /// // assert_eq!(b, (0b011000...).into());
+                #[doc = "assert_eq!(b, 2_" $size ".pow(" $bit_len "-1).into());"]
+                /// ```
+                pub fn shift_right_with(&mut self, bit: Bit) {
+                    self.bits >>= 1;
+                    if bit == Bit::One {
+                        self.bits |= 1 << ($bit_len-1);
+                    }
+                }
+            }
+
+            paste! {
+                /// Gets the bit at `index` and pass it to `func`, then set the bit at `index` to the result of `func`.
+                ///
+                /// # Errors
+                /// Will return [`Error::IndexOutOfRange`] if `index` is more than the number of bits.
+                ///
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0); // 0b0000"]
+                ///
+                /// // Here we invert the bit at index 3
+                /// b.get_and_set(3, |bit| bit.invert()).unwrap(); //0b1000
+                ///
+                /// assert_eq!(b, 0b1000.into())
+                /// ```
+                pub fn get_and_set<Func: FnOnce(Bit) -> Bit>(
+                    &mut self,
+                    index: $index_type,
+                    func: Func,
+                ) -> Result<(), Error> {
+                    let bit = self.get(index)?;
+                    self.set_unchecked(index, func(bit));
+                    Ok(())
+                }
+            }
+
+            paste!{
+                /// Gets the bit at `index` and pass it to `func`, then set the bit at `index` to the result of `func`.
+                ///
+                /// This method is the same as [`get_and_set`] but won't do a range check on `index`.
+                ///
+                #[doc = "[`get_and_set`]:" $name "::get_and_set"]
+                ///
+                /// # Panics
+                /// Will panic if `index` is more than the number of bits.
+                ///
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0); // 0b0000"]
+                ///
+                /// // Here we invert the bit at index 3. In this case it will go from 0 to 1
+                /// b.get_and_set_unchecked(3, |bit| bit.invert()); //0b1000
+                ///
+                /// assert_eq!(b, 0b1000.into())
+                /// ```
+                pub fn get_and_set_unchecked<Func: FnOnce(Bit) -> Bit>(
+                    &mut self,
+                    index: $index_type,
+                    func: Func,
+                ) {
+                    self.set_unchecked(index, func(self.get_unchecked(index)));
+                }
+            }
+
+            paste! {
+                /// Sets the bit at `index` to `bit`.
+                ///
+                /// # Errors
+                /// Will return [`Error::IndexOutOfRange`] if `index` is more than the number of bits.
+                ///
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0); // 0b0000"]
+                ///
+                /// // Here we set the bit at index 3 to 1
+                /// b.set(3, Bit::One).unwrap(); //0b1000
+                ///
+                /// assert_eq!(b, 0b1000.into())
+                /// ```
+                pub fn set(&mut self, index: $index_type, bit: Bit) -> Result<(), Error> {
+                    if (0..$bit_len).contains(&index) {
+                        self.set_unchecked(index, bit);
+                        Ok(())
+                    } else {
+                        Err(Error::IndexOutOfRange)
+                    }
+                }
+            }
+            paste!{
+                /// Sets the bit at `index` to `bit`.
+                ///
+                /// # Panics
+                /// Will panic if `index` is more than the number of bits.
+                ///
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0); // 0b0000"]
+                ///
+                /// // Here we set the bit at index 3 to 1
+                /// b.set(3, Bit::One); //0b1000
+                ///
+                /// assert_eq!(b, 0b1000.into())
+                /// ```
+                pub fn set_unchecked(&mut self, index: $index_type, bit: Bit) {
+                    match bit {
+                        Bit::Zero => self.bits &= !(1 << index),
+                        Bit::One => self.bits |= 1 << index,
+                    }
+                }
+            }
+
+            paste!{
+                /// Returns the bit at `index`.
+                ///
+                /// # Errors
+                /// Will return [`Error::IndexOutOfRange`] if `index` is more than the number of bits.
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                ///
+                #[doc = "let mut b = " $name "::new(0b0100);"]
+                ///
+                /// // Here we get the bit at index 2
+                /// let bit = b.get(2).unwrap();  //0b0100
+                ///
+                /// assert_eq!(bit, Bit::One);
+                /// ```
+                pub fn get(&self, index: $index_type) -> Result<Bit, Error> {
+                    if (0..$bit_len).contains(&index) {
+                        Ok(self.get_unchecked(index))
+                    } else {
+                        Err(Error::IndexOutOfRange)
+                    }
+                }
+            }
+
+            paste!{
+                /// Returns the bit at `index`.
+                ///
+                /// # Panics
+                /// Will panic if `index` is more than the number of bits.
+                ///
+                /// # Examples
+                /// ```
+                #[doc = "use bit_rw::{" $name ", Bit};"]
+                #[doc = "let mut b = " $name "::new(0b0100);"]
+                ///
+                /// // Here we get the bit at index 2
+                /// let bit = b.get_unchecked(2);  //0b0100
+                ///
+                /// assert_eq!(bit, Bit::One);
+                /// ```
+                pub fn get_unchecked(&self, index: $index_type) -> Bit {
+                    if self.bits & 1 << index == 0 {
+                        Bit::Zero
+                    } else {
+                        Bit::One
+                    }
+                }
             }
         }
 
-        pub fn get_and_set<F: FnOnce(Bit) -> Bit>(&mut self, index: $index_type, f: F) -> Result<(), Error> {
-            let bit = self.get(index)?;
-            self.set_unchecked(index, f(bit));
-            Ok(())
-        }
-
-        pub fn get_and_set_unchecked<F: FnOnce(Bit) -> Bit>(&mut self, index: $index_type, f: F) {
-            self.set_unchecked(index, f(self.get_unchecked(index)));
-        }
-
-        pub fn set(&mut self, index: $index_type, bit: Bit) -> Result<(), Error> {
-            if (0..(<$size>::BITS as $index_type)).contains(&index) {
-                self.set_unchecked(index, bit);
-                Ok(())
-            } else {
-                Err(Error::IndexOutOfRange)
+        impl From<$size> for $name {
+            fn from(bits: $size) -> Self {
+                Self::new(bits)
             }
         }
 
-        pub fn set_unchecked(&mut self, index: $index_type, bit: Bit) {
-            match bit {
-                Bit::Zero => self.bits &= !(1 << index),
-                Bit::One => self.bits |= 1 << index,
+        impl From<$name> for $size {
+            fn from(bits: $name) -> Self {
+                bits.bits
             }
         }
 
-        pub fn get(&self, index: $index_type) -> Result<Bit, Error> {
-             if (0..(<$size>::BITS as $index_type)).contains(&index) {
-                Ok(self.get_unchecked(index))
-            } else {
-                Err(Error::IndexOutOfRange)
+        impl PartialEq<Self> for $name {
+            fn eq(&self, other: &Self) -> bool {
+                self.bits == other.bits
             }
         }
 
-        pub fn get_unchecked(&self, index: $index_type) -> Bit {
-            if self.bits & 1 << index == 0 {
-                Bit::Zero
-            } else {
-                Bit::One
-            }
-        }
-    }
-
-    impl From<$size> for $name {
-        fn from(bits: $size) -> Self {
-            Self::new(bits)
-        }
-    }
-
-    impl From<$name> for $size {
-        fn from(bits: $name) -> Self {
-            bits.bits
-        }
-    }
-
-    impl PartialEq<Self> for $name {
-        fn eq(&self, other: &Self) -> bool {
-            self.bits == other.bits
-        }
-    }
-
-    impl Eq for $name {}
-    )*)
+        impl Eq for $name {}
+    };
 }
-
-impl_bits_for_size! { (Bits8, u8, u8) (Bits16, u16, u16) (Bits32, u32, u32) (Bits64, u64, u32) (Bits128, u128, u32) }
 
 macro_rules! impl_debug_for_bits {
-    ($(($name:ident, $bits_len:literal))*) => ($(
-    impl Binary for $name {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            f.write_fmt(format_args!(concat!("{:0", $bits_len, "b}"), &self.bits))
+    ($name:ident, $bits_len:literal) => {
+        impl Binary for $name {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                f.write_fmt(format_args!(concat!("{:0", $bits_len, "b}"), &self.bits))
+            }
         }
-    }
 
-    impl Debug for $name {
-        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-            f.write_str(concat!(stringify!($name), " { bits: "))?;
-            std::fmt::Binary::fmt(&self, f)?;
-            f.write_str(" }")
+        impl Debug for $name {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                f.write_str(concat!(stringify!($name), " { bits: "))?;
+                std::fmt::Binary::fmt(&self, f)?;
+                f.write_str(" }")
+            }
         }
-    }
-    )*)
+    };
 }
 
-impl_debug_for_bits! { (Bits8, 8) (Bits16, 16) (Bits32, 32) (Bits64, 64) (Bits128, 128) }
+macro_rules! impl_iterator_for_bits {
+    ($name:ident, $iterator_name:ident, $iterator_consuming_name:ident, $index_type:ty) => {
+        pub struct $iterator_name<'a> {
+            bits: &'a $name,
+            index: $index_type,
+        }
+
+        pub struct $iterator_consuming_name {
+            bits: $name,
+            index: $index_type,
+        }
+
+        impl<'a> Iterator for $iterator_name<'a> {
+            type Item = Bit;
+            fn next(&mut self) -> Option<Self::Item> {
+                self.index += 1;
+                self.bits.get(self.index - 1).ok()
+            }
+        }
+
+        impl<'a> Iterator for $iterator_consuming_name {
+            type Item = Bit;
+            fn next(&mut self) -> Option<Self::Item> {
+                self.index += 1;
+                self.bits.get(self.index - 1).ok()
+            }
+        }
+
+        impl<'a> IntoIterator for &'a $name {
+            type Item = Bit;
+            type IntoIter = $iterator_name<'a>;
+
+            fn into_iter(self) -> Self::IntoIter {
+                $iterator_name {
+                    bits: self,
+                    index: 0,
+                }
+            }
+        }
+
+        impl IntoIterator for $name {
+            type Item = Bit;
+            type IntoIter = $iterator_consuming_name;
+
+            fn into_iter(self) -> Self::IntoIter {
+                $iterator_consuming_name {
+                    bits: self,
+                    index: 0,
+                }
+            }
+        }
+    };
+}
+
+macro_rules! impl_all_bits {
+    ($(($name:ident, $bits_len:literal, $index_type:ty))*) => ($(
+        paste!{
+            impl_bits!($name, [<u $bits_len>], $index_type, $bits_len);
+            impl_debug_for_bits!($name, $bits_len);
+            impl_iterator_for_bits!($name, [<$name Iterator>], [<$name IteratorConsuming>], $index_type);
+        }
+    )*);
+}
+
+impl_all_bits! {
+    (Bits8, 8, u8)
+    (Bits16, 16, u16)
+    (Bits32, 32, u32)
+    (Bits64, 64, u32)
+    (Bits128, 128, u32)
+}
 
 #[cfg(test)]
 mod tests_bits64 {
@@ -279,10 +470,29 @@ mod tests_bits64 {
         }
     }
 }
+
 #[cfg(test)]
 mod tests_bits8 {
     use crate::Bit::*;
     use crate::*;
+
+    #[test]
+    fn iterator_test() {
+        let b = Bits8::new(0);
+        let mut count = 0;
+        for bit in &b {
+            count += 1;
+            assert_eq!(bit, Zero);
+        }
+        assert_eq!(b, 0.into());
+    
+        for bit in Bits8::new(!0) {
+            assert_eq!(bit, One);
+        }
+        // for bit in Bits8::new(0b1) {
+        //     assert_eq!(bit, One);
+        // }
+    }
 
     #[test]
     fn get_and_set_out_of_range() {
